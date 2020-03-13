991cfda7c08cd53db73f0b81c54a91d37a331d0a
LiamCJ <liamcjones2000@gmail.com>
2020-03-13 23:43:48 +0200

    simplesearchtest

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {MainContentComponent} from "./components/main-content/main-content.component";
 
+
 @Component({
     moduleId: module.id,
     selector: 'ss-app-root',
